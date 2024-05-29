0f309449ed79e3ff4220fc7e043b7593f8133d99
seer-lee <2seer@naver.com>
2024-05-30 00:21:49 +0900

    firstCommit

---
+++ src/test/java/com/inflearn/lecture/LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
